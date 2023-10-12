1da788fcfe36ca0e13a3bf086a01ec9d9f2f034d
b1ngoinho11 <budthbundit.thayaurai@gmail.com>
2023-10-13 02:49:23 +0700

    u

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import FileResponse
 from pathlib import Path
 
 app = FastAPI()
 
-video_directory = Path("video_files")
+directory = Path("video_files")
 
 @app.get("/videos/{video_filename}")
 async def get_video(video_filename: str):
-    video_path = video_directory / video_filename
+    video_path = directory / video_filename
 
     if not video_path.is_file():
         return {"error": "Video not found"}
 
     return FileResponse(video_path, headers={"Accept-Ranges": "bytes"})
+
+@app.post("/upload/")
+async def upload_file(file: UploadFile):
+    
+    with open(f"{directory}/{file.filename}", "wb") as f:
+        f.write(file.file.read())
