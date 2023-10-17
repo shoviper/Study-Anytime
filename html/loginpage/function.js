1d6479f1c2360a3b9a412c0babadc2cd91d594b1
shoviper <65011304@kmitl.ac.th>
2023-10-17 17:07:50 +0700

    s

--- html/loginpage/function.js
+++ html/loginpage/function.js
@@ -1,18 +1,63 @@
-//login page
-function studentlogin(){
+// //login page
+// function studentlogin(){
 
-}
+// }
 
-function lecturerlogin(){
+// function lecturerlogin(){
 
-}
+// }
 
 //signup page
 function signup(){
+    var username = document.getElementById("username");
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+    var confirmpassword = document.getElementById("confirmpassword");
 
+    if(password != confirmpassword){
+        alert("Error, please fill the password correctly.");
+        return;
+    }
+    
 }
 
-//resetpassword page
-function resetpassword(){
+// //resetpassword page
+// function resetpassword(){
     
+// }
+
+
+//test
+function studentlogin() {
+    if (isStudentSignedUp()) {
+        alert("Login as a student successful!");
+    } else {
+        alert("You need to sign up as a student first!");
+    }
+}
+
+function lecturerlogin() {
+    if (isLecturerSignedUp()) {
+        alert("Login as a lecturer successful!");
+    } else {
+        alert("You need to sign up as a lecturer first!");
+    }
+}
+
+function isStudentSignedUp() {
+    // Implement your logic to check if the user is signed up as a student
+    // For example, you can check if the user's email exists in a student database
+    var studentemail = document.getElementById("email").value; // Get the entered email
+    // Add your logic to check if the email exists in the student database
+    // For now, let's assume a simple check that the email contains "student"
+    return studentemail.includes("student");
+}
+
+function isLecturerSignedUp() {
+    // Implement your logic to check if the user is signed up as a lecturer
+    // Similar to the isStudentSignedUp function
+    var lectureremail = document.getElementById("email").value; // Get the entered email
+    // Add your logic to check if the email exists in the lecturer database
+    // For now, let's assume a simple check that the email contains "lecturer"
+    return lectureremail.includes("lecturer");
 }
