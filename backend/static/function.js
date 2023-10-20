3af63dafc8aba4cf4bb97e6534d84885c00c6985
shoviper <65011304@kmitl.ac.th>
2023-10-20 23:36:14 +0700

    chachakanokrak

--- backend/static/function.js
+++ backend/static/function.js
@@ -8,34 +8,8 @@
 // }
 
 //signup page
-function signup() {
-    var username = document.getElementById("username").value;
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    var confirmpassword = document.getElementById("confirmpassword").value;
-
-    if (password !== confirmpassword) {
-        alert("Error, please fill the password correctly.");
-        return;
-    }
-
-    var data = {
-        username: username,
-        email: email,
-        password: password
-    };
-
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', '/signup', true);
-    xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-    xhr.onload = function () {
-        if (xhr.status === 200) {
-            alert('Sign up successful');
-        } else {
-            alert('Sign up failed');
-        }
-    };
-    xhr.send(JSON.stringify(data));
+function login() {
+    alert("Hello");
 }
 
 
